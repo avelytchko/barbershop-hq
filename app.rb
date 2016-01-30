cc01cf62a2f647a966366a5d34fdc81293a59db8
Rand <avelytchko@gmail.com>
2016-01-30 17:17:27 +0200

    Keep values on post

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ set :database, 'sqlite3:barbershop.db'
 
 class Client < ActiveRecord::Base
   validates :name, presence: true
-  validates :phone, presence: true
   validates :datestamp, presence: true
+  validates :phone, presence: true
   validates :color, presence: true
 end
 
@@ -58,13 +58,16 @@ get '/secure/place' do
 end
 
 get '/visit' do
-
+  @c = Client.new
   erb :visit
 end
 
 post '/visit' do
-  c = Client.new params[:client]
-  c.save
-  erb :visit
-
+  @c = Client.new params[:client]
+  if @c.save
+    erb "<h2>Спасибо, вы записались!</h2>"
+  else
+    @error = @c.errors.full_messages.first
+    erb :visit
+  end
 end
