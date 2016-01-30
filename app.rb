09777b5556148072d47c7069ef8e90e359866d5e
Rand <avelytchko@gmail.com>
2016-01-30 17:49:58 +0200

    Add barber and client pages

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:barbershop.db'
 
 class Client < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
   validates :datestamp, presence: true
   validates :phone, presence: true
   validates :color, presence: true
@@ -70,4 +70,19 @@ post '/visit' do
     @error = @c.errors.full_messages.first
     erb :visit
   end
+end
+
+get '/barber/:id' do
+  @barber = Barber.find(params[:id])
+  erb :barber
+end
+
+get '/bookings' do
+  @clients = Client.order('created_at DESC')
+  erb :bookings
+end
+
+get '/client/:id' do
+  @client = Client.find(params[:id])
+  erb :client
 end
